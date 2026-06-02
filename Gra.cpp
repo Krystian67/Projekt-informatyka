a399d05a8440367b9c2ff2c3755f07e5fda79d9b
Krystian67 <eclas10@hotmail.com>
2026-06-02 08:46:38 +0200

    Plansza do gry

--- Gra.cpp
+++ Gra.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-using namespace std;
-int main (){
-    cout << "klops";
-
+void printBoard(const char board[9]) {
+    std::cout << "\n";
+    for (int i = 0; i < 9; i++) {
+        std::cout << ' ' << board[i] << ' ';
+        if (i % 3 != 2)
+            std::cout << '|';
+        if (i % 3 == 2 && i != 8)
+            std::cout << "\n\n";
+    }
+    std::cout << "\n\n";
 }
+
