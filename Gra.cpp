1792638a34cabdbf4c15d9663ff4baae14fd7530
Krystian67 <eclas10@hotmail.com>
2026-06-03 11:30:14 +0200

    Dalsze prace

--- Gra.cpp
+++ Gra.cpp
@@ -10,4 +10,16 @@ void printBoard(const char board[9]) {
     }
     std::cout << "\n\n";
 }
+bool checkWin(const char board[9], char player){
+    int lines [8][3] = {
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, 
+        {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, 
+        {0, 4, 8}, {2, 4, 6}             
+    };
+    for (int i=0; i<8; i++){
+        if (board[lines[i][0]] == player && board[lines[i][1]] == player && board[lines[i][2]] == player)
+            return true;
+    }
+    return false;
+}
 
