dd30e2ca2048d0858302464ef368d2c7c24afcfc
Krystian67 <eclas10@hotmail.com>
2026-06-09 08:45:46 +0200

    Duże zmiany, logika gry

--- Gra.cpp
+++ Gra.cpp
@@ -22,4 +22,42 @@ bool checkWin(const char board[9], char player){
     }
     return false;
 }
+bool boardFull(const char board[9]) {
+    for (int i = 0; i < 9; i++) {
+        if (board[i] == ' ')
+            return false;
+    }
+    return true;
+}
+int main () {
+    char board [9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    char currentPlayer = 'X';
+    int move;
+    std::cout << "Witaj w grze kółko i krzyżyk!\n";
+    std::cout << "Autor: Krystian Paetzke\n";
+    std::cout << "Gracz 1: X\nGracz 2: O\n";
+    std::cout << "Aby wykonać ruch, wpisz numer pola (1-9):\n";
+    std::cout << "1 | 2 | 3\n4 | 5 | 6\n7 | 8 | 9\n";
+    while (true) {
+        printBoard(board);
+        std::cout << "Ruch gracza " << currentPlayer << ": ";
+        std::cin >> move;
+        if (move < 1 || move > 9 || board[move - 1] != ' ') {
+            std::cout << "Nieprawidlowy ruch. Sprobuj ponownie.\n";
+            continue;
+        }
+        board[move - 1] = currentPlayer;
+        if (checkWin(board, currentPlayer)) {
+            printBoard(board);
+            std::cout << "Gracz " << currentPlayer << " wygrywa!\n";
+            break;
+        }
+        if (boardFull(board)) {
+            printBoard(board);
+            std::cout << "Remis!\n";
+            break;
+        }
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+}
 
